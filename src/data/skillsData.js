87e8083267e607ed8ab138536dea3a6abf5586e8
UIDAM <jan635573@gmail.com>
2025-02-21 16:20:32 +0900

    skills icon update

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -16,47 +16,47 @@ const skillsData = [
   },
   {
     id: 4,
-    name: 'React',
+    name: 'Sass',
     icon: '/images/about/skill-4.png'
   },
   {
     id: 5,
-    name: 'Vue',
+    name: 'React',
     icon: '/images/about/skill-5.png'
   },
   {
     id: 6,
-    name: 'MySQL',
+    name: 'Vue',
     icon: '/images/about/skill-6.png'
   },
   {
     id: 7,
-    name: 'Figma',
+    name: 'MySQL',
     icon: '/images/about/skill-7.png'
   },
   {
     id: 8,
-    name: 'Adobe XD',
+    name: 'Figma',
     icon: '/images/about/skill-8.png'
   },
   {
     id: 9,
-    name: 'Photoshop',
+    name: 'Adobe XD',
     icon: '/images/about/skill-9.png'
   },
   {
     id: 10,
-    name: 'Illustrator',
+    name: 'Photoshop',
     icon: '/images/about/skill-10.png'
   },
   {
     id: 11,
-    name: 'Lightroom',
+    name: 'Illustrator',
     icon: '/images/about/skill-11.png'
   },
   {
     id: 12,
-    name: 'Sass',
+    name: 'Lightroom',
     icon: '/images/about/skill-12.png'
   },
 ];
